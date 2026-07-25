8b1f8dc3a6b6e61048f2a9a8b9b68e2fc1521fd4
Issaqqq <kadavathepeedikayil786786@gmail.com>
2026-07-25 17:14:26 +0530

    TwoSum

--- Arrays/TwoSums.py
+++ Arrays/TwoSums.py
@@ -1,11 +1,30 @@
+# only for sorted arrays:
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         i = 0
+#         j = len(nums) - 1
+#         while (i<j):
+#             CurrentSum = nums[i] + nums[j]
+
+#             if(CurrentSum == target):
+#                 return [i,j]
+#             elif(CurrentSum < target):
+#                 i += 1
+#             else:
+#                 j -= 1
+#         return []
+    
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       hashmap = {}
-       for i in range(len(nums)):
-        hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]]
+        seen = {}
+
+        for i, num in enumerate(nums):
+            need = target - num
+
+            if need in seen:
+                return [seen[need], i]
+
+            seen[num] = i
 
         return []
